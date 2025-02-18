6f45600d7574d4234fc59414f2263f10a0074800
hsamudrala378 <hsamudrala2022@my.fit.edu>
2025-02-18 15:35:06 -0500

    Add files via upload

---
+++ NumberGuessingGame.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+bool isCorrectGuess = false;
+Random random = new Random();
+int randomNum = random.Next(1, 11);
+
+Console.WriteLine("Welcome to Number Guessing Game");
+Console.WriteLine("A number between 1 and 10 will be generated. ");
+Console.WriteLine("If you guess the correct number, you win!");
+
+while (!isCorrectGuess)
+{
+    Console.WriteLine("Please enter your guess.");
+    int guess = Convert.ToInt32(Console.ReadLine());
+
+    if(guess > randomNum)
+    {
+        Console.WriteLine("Your guess is too high.");
+
+    }
+
+    else if (guess < randomNum)
+    {
+        Console.WriteLine("Your guess is too low.");
+    }
+
+    else
+    {
+        Console.WriteLine("Correct!");
+        isCorrectGuess = true;
+    }
+}
+Console.WriteLine("Congratulations, you have won the game!");
+Console.ReadKey();
